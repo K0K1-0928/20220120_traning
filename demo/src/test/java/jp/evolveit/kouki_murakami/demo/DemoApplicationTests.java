69b327e67c1641e74a1a7e41f84179206fb412b9
Kouki Murakami <m.kouki.0928@gmail.com>
2022-01-20 14:29:42 +0900

    Add Spring project.

---
+++ demo/src/test/java/jp/evolveit/kouki_murakami/demo/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package jp.evolveit.kouki_murakami.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
